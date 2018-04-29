23f58d900b6c9668397a8538f84c4dcae49d559d
kanya <>
2018-04-29 12:36:49 +0700

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+
+class App extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: 'pink' }} />
+    )
+  }
+}
+
+export default App
